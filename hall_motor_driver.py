3a6935b32cf5592d50295cda2ebf16aa1a881189
Craig Moynes <craig.moynes@gmail.com>
2024-06-22 09:28:41 -0400

    Set, Seek and Find loop implemented for motor1

--- hall_motor_driver.py
+++ hall_motor_driver.py
@@ -1,24 +1,37 @@
 #Source: electrocredible.com, Language: MicroPython
 from machine import Pin
 import machine
+import random
 import time
 import asyncio
 
 DETECT_LEVEL = 5000  #Level below shows magnet detected
 
-hall_sensor1 = machine.ADC(26)
-hall_sensor2 = machine.ADC(27)
+hall_sensor1 = machine.ADC(27)
+hall_sensor2 = machine.ADC(26)
+
+AT = 0
+NEXT = 1
+DETECTED = 2
+motor1 = [None] * 3
+motor1[AT] = -1
+motor1[NEXT] = -1
+motor1[DETECTED] = False
 
 async def sensor_loop():
+    global motor1
     while True:
             value1 = hall_sensor1.read_u16()
-            detected1 = value1 < DETECT_LEVEL
+            detected1 = value1 < DETECT_LEVEL            
+            motor1[DETECTED] = detected1
             
             value2 = hall_sensor2.read_u16()
             detected2 = value2 < DETECT_LEVEL
-            print(f"Value: {value1}, {detected1}, {value2}, {detected2}")
             
-            await asyncio.sleep(0.5)
+            #print(f"Value: {value1}, {detected1}, {value2}, {detected2}")
+            
+            
+            await asyncio.sleep(0.01)
         
 # Define the pins for the stepper motor
 stepper_pins1 = [Pin(16, Pin.OUT), Pin(17, Pin.OUT), Pin(14, Pin.OUT), Pin(15, Pin.OUT)]
@@ -49,10 +62,43 @@ async def step(motor, direction, steps, delay):
         await asyncio.sleep(delay)
 # Set the initial step index to 0
 step_index = 0
+
+async def run_loop():
+    global motor1
+    while True:
+        if (motor1[AT] == -1):
+            print(f"motor1 AT: {motor1[AT]}, DETECTED: {motor1[DETECTED]}")
+            while motor1[DETECTED] == False:
+                print("seek STEP motor1 by 1")
+                await step(stepper_pins1, 1, 204.8 * 1, 0.002)
+            motor1[AT] = 0
+            print(f"motor1 AT: {motor1[AT]}, DETECTED: {motor1[DETECTED]}")
+        if motor1[AT] != motor1[NEXT]:
+            print(f"next STEP motor1 by 1, {motor1[AT]} vs {motor1[NEXT]}")
+            await step(stepper_pins1, 1, 204.8 * 1, 0.002)
+            motor1[AT] += 1
+            motor1[AT] = motor1[AT] % 10        
+        await asyncio.sleep(0.01)
+
+async def num_loop():
+    global motor1
+    while True:
+        #print(f"At: {motor1[AT]}, NEXT: {motor1[NEXT]}")
+        if motor1[AT] == motor1[NEXT]:
+            print("FOUND!")
+            await asyncio.sleep(1) 
+            motor1[NEXT] = random.randint(0,9)
+            print(f"Motor1 seeking to {motor1[NEXT]}")
+        await asyncio.sleep(0.1)
+
+motor1[NEXT] = 0
+
 # Take the specified number of steps in the anti-clockwise direction with a delay of 0.01 seconds between steps
 #asyncio.create_task(step(stepper_pins1, 1, 204.8 * 10, 0.002))
-asyncio.create_task(step(stepper_pins2, 1, 204.8 * 1, 0.002))
+#asyncio.create_task(step(stepper_pins2, 1, 204.8 * 4, 0.002))
+asyncio.create_task(run_loop())
 asyncio.create_task(sensor_loop())
+asyncio.create_task(num_loop())
 # Take the specified number of steps in the clockwise direction with a delay of 0.01 seconds between steps
 #step(-1, 1000, 0.001)
 
