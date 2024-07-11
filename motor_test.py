d15cb639b707944457289188f82d34fcd60a48fa
Craig Moynes <craig.moynes@gmail.com>
2024-07-11 12:29:35 -0400

    fix pinms and motor movements

--- motor_test.py
+++ motor_test.py
@@ -3,7 +3,7 @@ import utime
 from machine import Pin
 
 # Define the pins for the stepper motor
-stepper_pins = [Pin(12, Pin.OUT), Pin(13, Pin.OUT), Pin(14, Pin.OUT), Pin(15, Pin.OUT)]
+stepper_pins = [Pin(16, Pin.OUT), Pin(17, Pin.OUT), Pin(14, Pin.OUT), Pin(15, Pin.OUT)]
 
 # Define the sequence of steps for the motor to take
 step_sequence = [
@@ -31,6 +31,8 @@ def step(direction, steps, delay):
 # Set the initial step index to 0
 step_index = 0
 # Take the specified number of steps in the anti-clockwise direction with a delay of 0.01 seconds between steps
-step(1, 1536, 0.01)
+MOTOR_FACTOR=51.2
+for i in range(2):
+    step(1, 1.0 * MOTOR_FACTOR, 0.002)
 # Take the specified number of steps in the clockwise direction with a delay of 0.01 seconds between steps
 #step(-1, 1000, 0.001)
